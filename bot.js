d9d5e225c4d046d12bb7c5c44f5fda7eb5f97ee6
Andy <andyphung64@gmail.com>
2021-08-14 12:43:03 -0700

    add linter config file

--- bot.js
+++ bot.js
@@ -1,10 +1,19 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const token = require('./config.json');
+const fs = require('fs');
+
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('interactionCreate', interaction => {
+	console.log(interaction);
 });
 
 client.login(token);
