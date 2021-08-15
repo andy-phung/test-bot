eeefd20713d1a010c654e37169e8c0f36465adb6
Andy <andyphung64@gmail.com>
2021-08-15 10:18:46 -0700

    test interaction reply and followup

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,10 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong! 🏓');
+		await interaction.reply({ content: 'Pong!', ephemeral: true });
+		await new Promise(resolve => setTimeout(resolve, 3000)); // wait
+		await interaction.editReply({ content: '🏓', ephemeral: true });
+		await new Promise(resolve => setTimeout(resolve, 1000)); // wait
+		await interaction.followUp({ content: 'weeeeeeeeeeeeeeeeeee', ephemeral: true });
 	}
 };
