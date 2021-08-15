c383dce389053758bda3e9d98be61f1cdd43ec83
Andy <andyphung64@gmail.com>
2021-08-14 17:47:08 -0700

    add events folder

--- bot.js
+++ bot.js
@@ -4,15 +4,26 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('fs');
 
-
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+// clientId + this guild id and whatever other guild ids you want to use should be stored in config.json as a const and const array, respectively
+// note that doing things globally (across all guilds this bot has access to) is possible--see the guide
 const clientId = '872543813355315200';
 const guildId = '754243466241769512';
 
-
+// client initialization + logging in
+// remember to specify only the intents that you need
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.login(token);
+
+// fun stuff
+//client.user.setActivity('Bohemian Rhapsody', { type: 'LISTENING' });
+
+// grabbing events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+//grabbing commands from commands folder
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 client.commands = new Collection();
 
 for (const file of commandFiles) {
@@ -25,13 +36,7 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-client.once('ready', () => {
-	console.log('client ready');
-});
-
-client.login(token);
-
-
+// refreshing commands in the specified guild
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
@@ -47,6 +52,7 @@ client.login(token);
 	}
 })();
 
+// command handling
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -64,3 +70,13 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+// event handling
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
